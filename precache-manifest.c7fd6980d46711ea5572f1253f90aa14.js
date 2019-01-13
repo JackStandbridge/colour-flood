a596e5d47b613109a7627d797b174e63243559fb
Jack Standbridge <jack.s.standbridge@gmail.com>
2019-01-13 18:47:53 +0000

    Updates

---
+++ precache-manifest.c7fd6980d46711ea5572f1253f90aa14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22dd36f616c2ece8a4b1cc2217095003",
+    "url": "/colour-flood/static/media/Simplifica.22dd36f6.ttf"
+  },
+  {
+    "revision": "2fe32972f0d3e3c0ef87",
+    "url": "/colour-flood/static/js/runtime~main.2fe32972.js"
+  },
+  {
+    "revision": "c0b5dff108b03bcae054",
+    "url": "/colour-flood/static/js/main.c0b5dff1.chunk.js"
+  },
+  {
+    "revision": "e90ea2dee2aa977dbbc4",
+    "url": "/colour-flood/static/js/1.e90ea2de.chunk.js"
+  },
+  {
+    "revision": "c0b5dff108b03bcae054",
+    "url": "/colour-flood/static/css/main.342f2679.chunk.css"
+  },
+  {
+    "revision": "fff10da7180d063127efdaa7dbae0752",
+    "url": "/colour-flood/index.html"
+  }
+];
