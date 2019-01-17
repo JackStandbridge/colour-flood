074afaa9947bd68867da300a87632f5256802a63
Jack Standbridge <jack.s.standbridge@gmail.com>
2019-01-17 13:35:36 +0000

    adjust level select for greater clarity

--- src/components/Difficulty.js
+++ src/components/Difficulty.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
-export default ({ handleDifficulty }) => (
+export default ({ handleDifficulty, difficulty }) => (
   <div className='difficulty'>
-    <h1>Level</h1>
-    <button onClick={ () => handleDifficulty(4) }>4</button>
-    <button onClick={ () => handleDifficulty(5) }>5</button>
-    <button onClick={ () => handleDifficulty(6) }>6</button>
+    <h1>Colours:</h1>
+
+    <button
+      onClick={ () => handleDifficulty(4) }
+      disabled={ difficulty === 4 && 'disabled'}
+    >
+      4
+    </button>
+
+    <button
+      onClick={ () => handleDifficulty(5) }
+      disabled={ difficulty === 5 && 'disabled'}
+    >
+      5
+    </button>
+
+    <button
+      onClick={ () => handleDifficulty(6) }
+      disabled={ difficulty === 6 && 'disabled'}
+    >
+      6
+    </button>
   </div>
 )
--- src/containers/DifficultyContainer.js
+++ src/containers/DifficultyContainer.js
@@ -2,8 +2,10 @@ import { connect } from 'react-redux';
 import Difficulty from '../components/Difficulty';
 import { updateDifficulty } from '../data/actions/state-actions';
 
+const mapState = ({ difficulty, colour }) => ({ difficulty, colour });
+
 const mapDispatch = dispatch => ({
   handleDifficulty: difficulty => dispatch(updateDifficulty(difficulty)),
 });
 
-export default connect(null, mapDispatch)(Difficulty);
+export default connect(mapState, mapDispatch)(Difficulty);
